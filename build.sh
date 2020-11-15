b82302a02d8b68678528cde5d437d2f7ced0f2c4
Sebastian Pekarek <mail@sebbo.net>
2020-11-15 14:28:55 +0100

    fix(CI): multibranch gh-pages

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 npx tsc
 
-branch=${GITHUB_REF#refs/heads/}
-folder="./gh-pages/${branch}"
-if [ $branch != "develop" ] && [ $branch != "main" ]; then
-    echo "Skip documentation as branch is not develop and not main (is: ${branch}).";
+if [ $BRANCH != "develop" ] && [ $BRANCH != "main" ]; then
+    echo "Skip documentation as branch is not develop and not main (is: ${BRANCH}).";
     exit 0;
 fi;
 
@@ -18,7 +16,3 @@ mv ./mochawesome-report/mochawesome.html ./mochawesome-report/index.html
 mv ./mochawesome-report ./doc/tests
 
 npm run coverage
-
-mkdir -p $folder
-mv ./doc/* $folder/
-rm -rf ./doc
---
+++ release.config.js
@@ -0,0 +1,55 @@
+const plugins = [
+    ['@semantic-release/commit-analyzer', {
+        'preset': 'angular',
+        'releaseRules': [
+            {'type': 'refactor', 'release': 'patch'},
+            {'type': 'style', 'release': 'patch'},
+            {'type': 'build', 'scope': 'deps', 'release': 'patch'},
+            {'type': 'docs', 'release': 'patch'}
+        ],
+        'parserOpts': {
+            'noteKeywords': ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
+    }],
+    ['@semantic-release/release-notes-generator', {
+        'preset': 'angular',
+        'parserOpts': {
+            'noteKeywords': ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        },
+        'writerOpts': {
+            'commitsSort': ['subject', 'scope']
+        }
+    }],
+    ['@semantic-release/changelog', {
+        'changelogFile': 'CHANGELOG.md'
+    }],
+    '@semantic-release/npm',
+    '@semantic-release/github'
+];
+
+if(process.env.BRANCH === 'main') {
+    plugins.push(['@semantic-release/git', {
+        'assets': ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        'message': 'chore(release): :bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }]);
+}
+if(process.env.BRANCH === 'main' || process.env.BRANCH === 'develop') {
+    plugins.push(['@qiwi/semantic-release-gh-pages-plugin', {
+        'msg': 'docs: Updated for <%= nextRelease.gitTag %>',
+        'src': './gh-pages',
+        'dst': `./${process.env.BRANCH}`,
+        'pullTagsBranch': 'main'
+    }]);
+}
+
+module.exports = {
+    'branches': [
+        'main',
+        {
+            'name': 'develop',
+            'channel': 'next',
+            'prerelease': true
+        }
+    ],
+    'plugins': plugins
+};
